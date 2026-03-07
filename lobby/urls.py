92268da4d16c96a20ad2559093c8ed19c500667c
itsZling <micahgo425@gmail.com>
2026-03-07 15:55:21 -0500

    updated homepage, game page, header, and more styling

--- lobby/urls.py
+++ lobby/urls.py
@@ -4,8 +4,5 @@ from . import views
 urlpatterns = [
     path('game/', views.game, name='game.index'),
     path('results/', views.results, name='results'),
-    
-    # 2. Put the wildcard path LAST! 
-    # This way, it only catches actual random lobby codes.
     path('<str:lobby_id>/', views.index, name='lobby.index'), 
 ]
